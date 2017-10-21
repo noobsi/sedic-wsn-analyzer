de2802f3ceec7947901eadebc79c18f46abeb1b7
dandoh <dainhan605@gmail.com>
2017-10-21 15:17:12 +0700

    updates.

---
+++ Castalia/Castalia/src/node/application/simplePairApplication/SimplePairApplication.h
@@ -0,0 +1,42 @@
+/****************************************************************************
+ *  Copyright: National ICT Australia,  2007 - 2010                         *
+ *  Developed at the ATP lab, Networked Systems research theme              *
+ *  Author(s): Athanassios Boulis, Yuriy Tselishchev                        *
+ *  This file is distributed under the terms in the attached LICENSE file.  *
+ *  If you do not find this file, copies can be found by writing to:        *
+ *                                                                          *
+ *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia             *
+ *      Attention:  License Inquiry.                                        *
+ *                                                                          *
+ ****************************************************************************/
+
+#ifndef _SIMPLEPAIRAPPLICATION_H_
+#define _SIMPLEPAIRAPPLICATION_H_
+
+#include "VirtualApplication.h"
+
+using namespace std;
+
+enum SimplePairApplicationTimers {
+	SEND_PACKET = 1
+};
+
+class SimplePairApplication: public VirtualApplication {
+ private:
+	double packet_rate;
+	string recipientAddress;
+	double startupDelay;
+  bool isSource;
+  string sinkAddress;
+
+	float packet_spacing;
+	int dataSN;
+
+ protected:
+	void startup();
+	void fromNetworkLayer(ApplicationPacket *, const char *, double, double);
+	void handleRadioControlMessage(RadioControlMessage *);
+	void timerFiredCallback(int);
+};
+
+#endif				
